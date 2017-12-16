2d49d14195c7766dea139c75a53e40ee3b412da9
Irmen de Jong <irmen@razorvine.net>
2017-12-16 22:11:33 +0100

    second

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+
 source("get_dataset.R")
 source("combine.R")
 
@@ -12,10 +13,20 @@ interesting_columns <- column_names[grepl("mean..|std..|activityID|subjectID", c
 combined_data <- combined$combined_all[, interesting_columns, with=FALSE]   # data.table feature
 
 # 3. Use descriptive activity names to name the activities in the data set
-combined_data <- merge(combined$activities, interesting_data, by="activityID", all.y=TRUE)
+combined_data <- merge(combined$activities, combined_data, by="activityID", all.y=TRUE)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 # This has already been done in the combine function in step 1
 
 # 5. From the data set in step 4, creates a second, independent tidy data set
 # with the average of each variable for each activity and each subject.
+library(tibble)
+library(dplyr)
+summary <-
+    as_tibble(combined_data) %>%
+    group_by(subjectID, activityname) %>%
+    summarize_all(mean)
+
+# write the dataset, 
+message("writing tidy summary dataset")
+fwrite(summary, file="second_dataset_tidy.csv")
